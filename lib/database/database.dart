9d2b89b0199716506d652a74347458b3b5525727
Pradip Jadav <77873775+Pradipjadav05@users.noreply.github.com>
2023-10-06 12:23:37 +0530

    Generate Code

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:floor/floor.dart';
 import 'package:floor_database/database/entity/user_table.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+part 'database.g.dart';
 
 @Database(version: 1, entities: [UserTable])
 abstract class AppDatabase extends FloorDatabase {
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,24 @@
+import 'package:floor/floor.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'database/database.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+// final databasd = AppDatabase();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
